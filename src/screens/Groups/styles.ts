8dbed2ca7b8d832f89f2a1372465f18bc54c2875
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-03-26 16:34:59 -0300

    safeareaview

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled, { css } from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   ${({ theme }) => css`
     flex: 1;
     background-color: ${theme.colors.gray_600};
--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,7 +1,8 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled, { css } from "styled-components/native";
 import { UsersThree } from "phosphor-react-native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   ${({ theme }) => css`
     flex: 1;
     background-color: ${theme.colors.gray_600};
