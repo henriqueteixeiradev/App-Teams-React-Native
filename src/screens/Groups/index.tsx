f459b11ca81d8314500a9efc3169db837ec8bc2e
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-03-26 08:03:07 -0300

    flatlist no cardgroup e criação dos states do groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { HightLight } from "@components/HightLight";
@@ -5,11 +7,20 @@ import { HightLight } from "@components/HightLight";
 import * as S from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Biricu",
+    "Galera do Soppa",
+  ]);
+
   return (
     <S.Container>
       <Header />
       <HightLight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do biricu" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   );
 }
