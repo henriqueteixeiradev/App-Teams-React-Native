276d7231a0314d45a0d3c35fba345feca4759692
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-03-26 22:39:01 -0300

    filtrando por time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Alert } from "react-native";
+import * as S from "./styles";
 import { useRoute } from "@react-navigation/native";
 import { FlatList } from "react-native";
 import { ButtonIcon } from "@components/ButtonIcon";
@@ -6,30 +8,67 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { HightLight } from "@components/HightLight";
 import { Input } from "@components/Input";
-import * as S from "./styles";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
   group: string;
 };
 
 export function Players() {
-  const [team, setTeam] = useState("");
-  const [players, setPlayers] = useState<string[]>([
-    "Henrique",
-    "Iolanda",
-    "Karolyna",
-    "Mikael",
-    "Quitéria",
-    "Bruno",
-    "Buluga",
-  ]);
+  const [newPlayerName, setNewPlayerName] = useState("");
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
   const route = useRoute();
 
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (
+      newPlayerName.trim() === undefined ||
+      newPlayerName.trim() === null ||
+      newPlayerName.trim() === ""
+    ) {
+      return Alert.alert("Ooops", "Informe o nome da pessoa para adicionar.");
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Ooops", error.message);
+      } else {
+        Alert.alert("Ooops", "Não foi possível adicionar a pessoa.");
+      }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team);
+      setPlayers(playersByTeam);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ooops", "Não foi possível carregar os jogadores.");
+    }
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+
+    fetchPlayersByTeam();
+  }, [team]);
 
   return (
     <S.Container>
@@ -40,21 +79,18 @@ export function Players() {
       />
 
       <S.Form>
-        <Input placeholder="Nome do jogador" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          placeholder="Nome do jogador"
+          autoCorrect={false}
+          value={newPlayerName}
+          onChangeText={setNewPlayerName}
+        />
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </S.Form>
 
       <S.HeaderList>
         <FlatList
-          data={[
-            "Time A",
-            "Time B",
-            "Time C",
-            "Time D",
-            "Time E",
-            "Time F",
-            "Time G",
-          ]}
+          data={["Time A", "Time B", "Time C"]}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <Filter
@@ -71,9 +107,9 @@ export function Players() {
 
       <FlatList
         data={players}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() => {}} />
+          <PlayerCard name={item.name} onRemove={() => {}} />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Não há pessoas nesse time" />
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = "@teams:groups";
+const PLAYER_COLLECTION = "@teams:players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
