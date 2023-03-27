147ea4aa7d750af2b4e9577d1f3cd824b7e77cbb
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-03-26 21:14:40 -0300

    abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -34,6 +34,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchGroups();
@@ -47,7 +51,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
